f0c27896e923ff5e8e7e0b56b62d00d6fa08e3d5
sergey_panteleenko <depstor@vk.com>
2025-10-18 00:59:04 +0300

    showMenu-v1.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 import '/src/sass/style.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const showMenu = () => {
+        try {
+            const gamburger = document.querySelector('.header__gamburger');
+            const mobileNav = document.querySelector('.header__nav-mobile ');
+
+            gamburger.addEventListener('click', () => {
+                gamburger.classList.toggle('active')
+                mobileNav.classList.toggle('active')
+            })
+
+            document.body.style.overflow = mobileNav.classList.contains('active') ? 'hidden' : '';
+        } catch (error) {
+            console.error('Error initializing mobile menu:', error.message);
+        }
+
+    };
+
+    showMenu()
+
+});
